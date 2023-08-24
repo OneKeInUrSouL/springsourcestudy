7e70de756012477264d1da6f23fb10581e7db052
OneKeInUrSouL <FengChi.Dan@cisdi.com.cn>
2023-08-24 22:51:51 +0800

    ci:增加注解

--- src/main/java/com/example/springsourcestudy/pojo/UserFactoryBean.java
+++ src/main/java/com/example/springsourcestudy/pojo/UserFactoryBean.java
@@ -8,6 +8,7 @@ public class UserFactoryBean implements FactoryBean<User> {
     // 它用于创建和配置对象，以及将它们添加到Spring容器中。
     // FactoryBean的主要作用是将对象的创建和配置逻辑封装起来，使得开发者可以以更高级别的抽象来定义和配置Bean。
     // 它允许你在创建Bean时执行一些自定义的逻辑，而不仅仅是简单地实例化一个对象。
+    // FactoryBean将实例注入到容器时会注入两部分内容，一个是创建的实例化内容，另一个是其FactoryBean本身
     @Override
     public User getObject() throws Exception {
         //忽略复杂逻辑
